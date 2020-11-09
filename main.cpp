0e20c98c3eb0d2bcb621eb94e7f91447e9835e3a
denvitiuk <denvittiuk@gmail.com>
2020-11-09 13:20:39 +0200

    second commit-example for good mark

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    for (int count = 0; count < 10; ++count)
+        std::cout << count << " ";
+
     return 0;
 }
-/
